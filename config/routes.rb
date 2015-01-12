f1a44f089a24f33c2e52955231e8156073350ef7
abhishekbhan <abbhan@gmail.com>
2015-01-12 15:19:51 -0800

    Install devise gem and setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   resources :articles#, only: [:new, :create, :destroy]
   get 'welcome/index'# => 'welcome#index'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
